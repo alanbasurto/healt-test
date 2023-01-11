ed46affc2f454f16c4884ee493742f4d4aeac356
Alan Basurto <alanrbasurto@gmail.com>
2023-01-11 11:37:26 -0600

    add a simple network connectivity

--- all_checks.py
+++ all_checks.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import shutil
+import socket
 
 def check_reboot():
 	"""just updating a commetn"""
@@ -20,10 +21,19 @@ def check_root_full():
 		"""Returns true if the rooot partition is full, flase otherwise"""
 		return check_disk_full(disk="/",min_gb=2,min_percent=10)
 
+def check_no_network():
+	"""Return True if it fails to resolve google's URL"""
+	try:
+		socket.gethostbyname("wwww.google.com")
+		return False
+	except:
+		return True
+
 def main():
 	checks = [
 		(check_reboot,"Pending Reboot"),
-		(check_root_full,"Root partition full")
+		(check_root_full,"Root partition full"),
+		(check_no_network,"No working network")
 	]
 	everything_ok = True
 	for check,msg in checks:
